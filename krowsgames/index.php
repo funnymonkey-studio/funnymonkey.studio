074fc470828d4d03a0eb8381b1923f97e54c89cc
Joe-vis <69452277+Joe-vis@users.noreply.github.com>
2021-02-08 21:00:42 +0200

    Add files via upload
    :)

---
+++ krowsgames/index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="../css/style.css">
+        <title>MONKE</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <?php
+            require "../external/header.php"
+        ?>
+    </head>
+    <body>
+        <?php
+            require "../external/navbar.php"
+        ?>
+        <div class="center">
+        <ul>
+            <li><a href="https://www.dropbox.com/sh/c1sevd578521qkz/AABpGfo2wBTVCZBh3mpWPyb2a?dl=0">zom 0.3.6</a></li>
+        </ul>
+        </div>
+    </body>
+</html>
